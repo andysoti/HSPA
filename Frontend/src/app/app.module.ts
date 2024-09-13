ae357285d57c7a6c7d112eefc6b45dd4b4913789
andysoti <andrew.goetz@soti.net>
2024-09-13 15:58:49 -0400

    routing setup, add-property route added

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 // import {HttpClientModule } from '@angular/common/http';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,17 +10,25 @@ import { PropertyListComponent } from './property/property-list/property-list.co
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { HttpClientModule, provideHttpClient } from '@angular/common/http';
 import { HousingService } from './services/housing.service';
+import { AddPropertyComponent } from './property/add-property/add-property.component';
+
+const appRoutes: Routes = [
+  {path: 'add-property', component: AddPropertyComponent},
+  {path: '', component: PropertyListComponent}
+]
 
 @NgModule({
   declarations: [	
     AppComponent,
     PropertyCardComponent,
     PropertyListComponent,
-      NavBarComponent
+      NavBarComponent,
+      AddPropertyComponent
    ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
     // HttpClientModule
   ],
   providers: [provideHttpClient(), HousingService],
