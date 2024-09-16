18f74999965ad7329075ca6d80151d26bcc82753
andysoti <andrew.goetz@soti.net>
2024-09-16 11:29:23 -0400

    Animation fixed!, resuming video 13.

--- Frontend/src/app/property/property-card/property-card.component.ts
+++ Frontend/src/app/property/property-card/property-card.component.ts
@@ -7,22 +7,8 @@ import { animate, state, style, transition, trigger } from "@angular/animations"
     // template: `<h1>I am a card</h1>`,
     templateUrl: 'property-card.component.html',
     // styles: [`h1 {font-weight: normal;}`]
-    styleUrls: ['property-card.component.html'],
-    // card anim: here, class, and html card wrapper
-    // animations: [
-    //     trigger('scaleAnimation', [
-    //         state('false', style({opacity: '1'})),
-    //         state('true', style({opacity: '1'})),
-    //            // // transform: 'scale(1.2,1.2)',
-    //            // // transition-duration: '500ms',
-    //            // // transition-timing-function: 'ease-out'}))
-    //         transition('false => true', [
-    //             style({transform: 'scale(1.3,1.3)', position: 'absolute' }),
-    //             animate('5ms ease-out', style({transform: 'scale(1.2,1.2)', position: 'absolute' }))
-    //                         ])
-    //                     ]
-    //                 )
-    //             ]
+    styleUrls: ['property-card.component.css'],
+
 })
 
 
@@ -32,12 +18,6 @@ export class PropertyCardComponent {
 property!: IProperty;
 
 
-// public fadeInStart = false;
-//     // constructor() {this.fadeInStart = true;};
-//     @HostListener('mouseenter')
-//     onMouseEnter(){
-//         this.fadeInStart = true;
-//     }
 
     
             
