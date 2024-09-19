f766f8f92a4558e73f621c8be3db666b68e03510
andysoti <andrew.goetz@soti.net>
2024-09-19 14:33:13 -0400

    fixed the issue of basic info problem

--- Frontend/src/app/property/add-property/add-property.component.ts
+++ Frontend/src/app/property/add-property/add-property.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { TabsetComponent } from 'ngx-bootstrap/tabs';
 import { IProperty } from '../../model/iproperty';
@@ -11,8 +11,13 @@ import { IPropertyBase } from '../../model/iPropertybase';
   styleUrls: ['./add-property.component.css']
 })
 export class AddPropertyComponent implements OnInit {
-  @ViewChild('Form') addPropertyForm: NgForm | undefined;
+[x: string]: any;
+  // @ViewChild('Form') addPropertyForm: NgForm | undefined;
   @ViewChild('formTabs') formTabs?: TabsetComponent;
+
+  addPropertyForm!: FormGroup;
+  NextClicked: boolean = false;
+
 
   propertyTypes: Array<string> = ['House', "Apartment", "Duplex"]
   furnishTypes: Array<string> = ['Fully', "Semi", "Unfurnished"]
@@ -27,11 +32,46 @@ export class AddPropertyComponent implements OnInit {
   };
 
 
-  constructor(private router: Router) { }
-
+  constructor(private fb: FormBuilder, private router: Router) { }
   ngOnInit() {
+    this.CreateAddPropertyForm();
+  }
 
+  CreateAddPropertyForm() {
+    this.addPropertyForm = this.fb.group({
+      BasicInfo: this.fb.group({
+          SellRent: ['1', Validators.required],
+          BHK: [null, Validators.required],
+          PType: [null, Validators.required],
+          FType: [null, Validators.required],
+          Name: [null, Validators.required],
+          City: [null, Validators.required]
+      }),
 
+      PriceInfo: this.fb.group({
+          Price: [null, Validators.required],
+          BuiltArea: [null, Validators.required],
+          CarpetArea: [null],
+          Security: [0],
+          Maintenance: [0],
+      }),
+
+      AddressInfo: this.fb.group({
+          FloorNo: [null],
+          TotalFloor: [null],
+          Address: [null, Validators.required],
+          LandMark: [null],
+      }),
+
+      OtherInfo: this.fb.group({
+          RTM: [null, Validators.required],
+          PossessionOn: [null, Validators.required],
+          AOP: [null],
+          Gated: [null],
+          MainEntrance: [null],
+          Description: [null]
+      })
+  });
   }
 
   
@@ -40,15 +80,93 @@ export class AddPropertyComponent implements OnInit {
   }
 
   onSubmit(){
+    this.NextClicked = true;
+    if (this.BasicInfo.invalid) {
+      this.formTabs!.tabs[0].active = true; // activate that tab
+      return;
+  }
+
+  if (this.PriceInfo.invalid) {
+      this.formTabs!.tabs[1].active = true;
+      return;
+  }
+
+  if (this.AddressInfo.invalid) {
+      this.formTabs!.tabs[2].active = true;
+      return;
+  }
+
+  if (this.OtherInfo.invalid) {
+      this.formTabs!.tabs[3].active = true;
+      return;
+  }
+
     console.log('Form, submitted.');
     console.log('SellRent=' + this.addPropertyForm!.value.BasicInfo.SellRent)
     console.log(this.addPropertyForm);
   }
 
-  selectTab(tabId: number) {
-    if (this.formTabs?.tabs[tabId]) {
-      this.formTabs.tabs[tabId].active = true;
+  selectTab(NextTabId: number, IsCurrentTabValid: boolean) {
+    this.NextClicked = true;
+    if (IsCurrentTabValid) {
+        this.formTabs!.tabs[NextTabId].active = true;
+        this.NextClicked = false; // not needed
     }
-  }
+}
+
+  //getter
+
+  get BasicInfo() {
+    return this.addPropertyForm.controls['BasicInfo'] as FormGroup;
+}
+
+get PriceInfo() {
+    return this.addPropertyForm.controls['PriceInfo'] as FormGroup;
+}
+
+get AddressInfo() {
+    return this.addPropertyForm.controls['AddressInfo'] as FormGroup;
+}
+
+get OtherInfo() {
+    return this.addPropertyForm.controls['OtherInfo'] as FormGroup;
+}
+
+// control getters
+get SellRent() {
+  return this.BasicInfo.controls['SellRent'] as FormControl;
+}
+
+get BHK() {
+  return this.BasicInfo.controls['BHK'] as FormControl;
+}
+
+get PType() {
+  return this.BasicInfo.controls['PType'] as FormControl;
+}
+
+get FType() {
+  return this.BasicInfo.controls['FType'] as FormControl;
+}
+
+get Name() {
+  return this.BasicInfo.controls['Name'] as FormControl;
+}
+
+get City() {
+  return this.BasicInfo.controls['City'] as FormControl;
+}
+
+get Price() {
+  return this.PriceInfo.controls['Price'] as FormControl;
+}
+
+get BuiltArea() {
+  return this.PriceInfo.controls['BuiltArea'] as FormControl;
+}
+
+get CarpetArea() {
+  return this.PriceInfo.controls['CarpetArea'] as FormControl;
+}
 
 }
