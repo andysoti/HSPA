e8252a5a725d2035356c1eef842ffed0d10ff83d
Andrew Goetz <Andrew.Goetz@soti.net>
2024-10-01 12:50:24 -0400

    Video 54 done. Retry to connect to server on 500 error

--- Frontend/src/app/services/httperror-interceptor.service.ts
+++ Frontend/src/app/services/httperror-interceptor.service.ts
@@ -1,8 +1,10 @@
 import { HttpErrorResponse, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import {throwError } from 'rxjs';
-import { catchError } from 'rxjs/operators';
+import {Observable, of, throwError } from 'rxjs';
+import { catchError, concatMap, retry, retryWhen } from 'rxjs/operators';
+// import { ErrorCode } from '../enums/enums';
 import { AlertifyService } from './alertify.service';
+import { ErrorCode } from '../enums/enums';
 @Injectable({
     providedIn: 'root'
 })
@@ -12,6 +14,8 @@ export class HttpErrorInterceptorService implements HttpInterceptor {
         console.log('HTTP Request started');
         return next.handle(request)
             .pipe(
+              // retry(10),
+              retryWhen(error => this.retryRequest(error,10)),
                 catchError((error: HttpErrorResponse) => {
                     const errorMessage = this.setError(error);
                     console.log(error);
@@ -33,4 +37,26 @@ export class HttpErrorInterceptorService implements HttpInterceptor {
         }
         return errorMessage;
     }
+
+    retryRequest(error : any, retryCount: number): Observable<unknown>
+    {
+        return error.pipe(
+            concatMap((checkErr: HttpErrorResponse, count: number) => {
+                if(count <= retryCount)
+                {
+                    switch(checkErr.status)
+                    {
+                    case ErrorCode.serverDown :
+                        return of(checkErr);
+                    // case ErrorCode.unauthorised :
+                    //     return of(checkErr);
+                    }
+                }
+                return throwError(checkErr);
+            })
+        );
+    }
+
+
+
 }
--- WebAPI/Controllers/AccountController.cs
+++ WebAPI/Controllers/AccountController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
 using WebAPI.Dtos;
 using WebAPI.Errors;
+using WebAPI.Extensions;
 using WebAPI.Interfaces;
 using WebAPI.Models;
 namespace WebAPI.Controllers
@@ -52,8 +53,9 @@ namespace WebAPI.Controllers
             
             ApiError apiError = new ApiError();
 
-            if(string.IsNullOrEmpty(loginReq.UserName.Trim()) || 
-                string.IsNullOrEmpty(loginReq.Password.Trim())) {
+            // if(string.IsNullOrEmpty(loginReq.UserName.Trim()) || 
+            //     string.IsNullOrEmpty(loginReq.Password.Trim())) {
+            if(loginReq.UserName.IsEmpty() || loginReq.Password.IsEmpty()) {
                     apiError.ErrorCode=BadRequest().StatusCode;
                     apiError.ErrorMessage="User name or password can not be blank";                    
                     return BadRequest(apiError);
